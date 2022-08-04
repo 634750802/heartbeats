6503cf1585e97da41ce84bcf9ef1529ce86cdf94
Jagger <634750802@qq.com>
2022-08-04 11:19:41 +0800

    fix config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,7 @@ import svgLoader from "vite-svg-loader";
  * @type {import('vitepress').UserConfig}
  */
 const config = {
+    base: '/heartbeats/',
     vite: {
         plugins: [svgLoader()]
     }
